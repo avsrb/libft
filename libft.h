b9b8ffd6a70c5cb279b5cc19abb8a489cc392ede
mshmelly <mshmelly@student.21-school.ru>
2021-04-28 11:18:28 +0300

    update header and Makefile

--- libft.h
+++ libft.h
@@ -55,5 +55,6 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+char	*ft_strnew(size_t size);
 
 #endif
